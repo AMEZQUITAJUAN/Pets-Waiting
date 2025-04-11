eaa0f6aaee25f7f5c75089f755ceeda85d2f66dc
dannyy23 <dannyinampues27@gmail.com>
2025-04-11 07:48:20 -0500

    udusrio1

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frm_product');
+    return view('frm_usuario');
     return "Bienvenidos a Pets-Waiting";
 });
 
