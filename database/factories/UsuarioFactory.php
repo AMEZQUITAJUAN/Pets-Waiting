17492e25fbb65a1a18d38bf55e2c861dd2b1ada8
AMEZQUITAJUAN <amezquitadussan@gmail.com>
2025-04-23 18:41:50 -0500

    factory

---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
+ */
+class UsuarioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->name(), // Genera un nombre aleatorio
+            'email' => $this->faker->unique()->safeEmail(), // Genera un email único
+            'password' => bcrypt('password'), // Contraseña encriptada
+            'telefono' => $this->faker->optional()->phoneNumber(), // Teléfono opcional
+        ];
+    }
+}
