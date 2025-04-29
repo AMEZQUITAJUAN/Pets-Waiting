d7ca1d241d1106812a85baff0577f2a8e9015a6a
AMEZQUITAJUAN <amezquitadussan@gmail.com>
2025-04-29 18:37:16 -0500

    arreglarShow

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,7 @@ Route::get('prueba', function () {
 });
 
 // Rutas para Mascotas
-Route::get('mascotas', [MascotasController::class, 'index'])->name('mascotas.index'); // Mostrar lista de mascotas
-Route::get('mascotas/create', [MascotasController::class, 'create'])->name('mascotas.create'); // Mostrar formulario de creación de mascota
-Route::get('mascotas/{id}', [MascotasController::class, 'show'])->name('mascotas.show'); // Mostrar detalles de una mascota
-Route::post('mascotas', [MascotasController::class, 'store'])->name('mascotas.store'); // Guardar mascota
-Route::get('mascotas/{mascota}/edit', [MascotasController::class, 'edit'])->name('mascotas.edit'); // Mostrar formulario de edición de mascota
-Route::put('mascotas/{mascota}', [MascotasController::class, 'update'])->name('mascotas.update'); // Actualizar mascota
-Route::delete('mascotas/{mascota}', [MascotasController::class, 'destroy'])->name('mascotas.destroy'); // Eliminar mascota
+Route::resource('mascotas', MascotasController::class);
 
 // Rutas para Usuarios
 Route::post('usuarios/store', [UsuariosController::class, 'store'])->name('usuarios.store'); // Guardar usuario
